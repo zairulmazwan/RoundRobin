7123bd59d0e151511a00c50151082fd877721c34
zairulmazwan <zairulmazwan@gmail.com>
2024-10-23 12:21:57 +0100

    push

--- RoundRobin/Process.cs
+++ RoundRobin/Process.cs
@@ -3,12 +3,14 @@ public class Process
     string processName;
     int burstTime;
     int remainingTime;
+    int waitingTime;
 
     public Process (string name, int bt)
     {
         processName = name;
         burstTime = bt;
         remainingTime = bt;
+        waitingTime = 0;
     }
 
     public List<Process> genDataset(int n)
@@ -33,5 +35,15 @@ public class Process
         Console.WriteLine("Burst time remaining : "+remainingTime);
     }
 
+}
 
+public class RoundRobin 
+{
+    Queue<Process> queue = new Queue<Process>();
+    int tiimeSlot = 3;
+
+    public void insertProcess(Process x)
+    {
+
+    }
 }
