783507e153605283abf7b5c56fcbcdd73d617b87
zairulmazwan <zairulmazwan@gmail.com>
2024-10-24 20:43:24 +0100

    24/10/2024

--- RoundRobin/Program.cs
+++ RoundRobin/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-// Process p1 = new Process("Process 1", 15);
+﻿// Process p1 = new Process("Process 1", 15);
 // p1.printProcessInfo();
 // p1.updateRemaining(3);
 // p1.printProcessInfo();
